26de3bc40fd173fdc21327577a112c33404effbb
alvin-c-cruz <alvinccruz12@gmail.com>
2021-11-06 20:40:33 +0700

    Added address to dataclass.py

--- blueprints/vendor/dataclass.py
+++ blueprints/vendor/dataclass.py
@@ -1,14 +1,15 @@
 from dataclasses import dataclass
 from ... packages import SimpleEntry
+from sqlite_data_model import sqliteDataModel
 
 @dataclass
-class Vendor(SimpleEntry):
-    name: str = None
-    tin: str = None
+class Vendor(sqliteDataModel):
+	name: str = None
+	tin: str = None
+	address: str = None
 
-
-    def is_related(self):
-    	for voucher in ('cd',):
-    		if self.db.execute('SELECT COUNT(*) FROM tbl_cd WHERE vendor_id = ?;', (self.id, )).fetchone()[0]:
-    			return True
-    	return False
+	def is_related(self):
+		for voucher in ('cd',):	
+			if self.db.execute('SELECT COUNT(*) FROM tbl_cd WHERE vendor_id = ?;', (self.id, )).fetchone()[0]:
+				return True
+		return False
